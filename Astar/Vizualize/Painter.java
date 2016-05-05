e5296fbd6a0eccfa82f67c91079ffd7c99b4246f
xvaisr <xvaisr00@stud.fit.vutbr.cz>
2016-05-05 22:26:11 +0200

    Uploading classes required for vizualization

---
+++ Astar/Vizualize/Painter.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Astar.Vizualize;
+
+import Astar.Grid;
+import Astar.IStack;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+/**
+ *
+ * @author Speeddrat
+ */
+class Painter {
+    private final Color[][] colorGrid = new Color[40][40];
+    private static final Color GRID = Color.BLACK;
+    private static final Color OBSTACLE = Color.GRAY;
+    private static final Color FREE = Color.WHITE;
+    private static final Color START = Color.BLUE;
+    private static final Color DESTINATION = Color.RED;
+    private static final Color PATH = Color.GREEN;
+    private static final int SIZE = 10;
+
+    public Painter(Point start, Point destination, Grid grid, IStack<Point> path) {
+        for (int x = 0; x < 40; x++) {
+            for (int y = 0; y < 40; y++) {
+                if (!grid.getIsFree(x, y))
+                    colorGrid[x][y] = OBSTACLE;
+                else colorGrid[x][y] = FREE;
+            }
+        }
+        
+        while(!path.empty()) {
+            Point p = path.getPeek();
+            colorGrid[p.x][p.y] = PATH;
+            path = path.pop();
+        }
+        
+        colorGrid[start.x][start.y] = START;
+        colorGrid[destination.x][destination.y] = DESTINATION;
+    }
+    
+    
+
+    void paintContent(Graphics g) {
+        g.setColor(FREE);
+        g.fillRect(0, 0, 500, 500);
+        
+        
+        g.setColor(GRID);
+        
+        for (int i = 0; i <= 40; i++) {
+            int x = i*11;    
+            g.drawLine(x, 0, x, 440);
+        }
+        
+        for (int i = 0; i <= 40; i++) {
+            int y = i*11;    
+            g.drawLine(0, y, 440, y);
+        }
+        
+        for (int x = 0; x < 40; x++) {
+            for (int y = 0; y < 40; y++) {
+                Color c = colorGrid[x][y];
+                if (c != Color.WHITE) {
+                    g.setColor(c);
+                    g.fillRect(x*(SIZE + 1), y*(SIZE + 1), SIZE, SIZE);
+                }                    
+            }
+        }
+        
+    }
+    
+}
---
+++ Astar/Vizualize/Vizualize.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Astar.Vizualize;
+import Astar.Grid;
+import Astar.IStack;
+import Astar.ImutableStack;
+import java.awt.Canvas;
+import java.awt.Point;
+/**
+ *
+ * @author Speeddrat
+ */
+public class Vizualize {
+    private static final Vizualize instance = new Vizualize();
+    private Window w;
+    
+    private Vizualize() {
+        this.w = new Window();
+        Screen s = new Screen(this.getPainter());
+        w.setScreen(s);
+    }
+    
+    public static Vizualize getInstance() {
+        return instance;
+    }
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                Vizualize instance;
+                instance = Vizualize.getInstance();
+                instance.run();
+            }
+        });
+    }
+
+    private Painter getPainter() {
+        Grid g = new Grid(40, 40);
+        IStack<Point> path;// = new ImutableStack();
+        Point start, dest;
+        
+        // Point(x, y);
+        start = new Point(0, 0);
+        dest = new Point(39, 39);
+        
+        path = Astar.Astar.getPath(start, dest, g);
+        
+        Grid.setWorld2(g);
+        return new Painter(start, dest, g, path);
+        
+    }
+
+    private void run() {
+        java.awt.EventQueue.invokeLater(this.w);
+    }
+}
